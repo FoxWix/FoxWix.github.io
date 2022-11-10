6158e5da265955c7e4a02df1ab6f2adc4e3aeae0
AAWPE <74542941+AAWPE@users.noreply.github.com>
2022-11-10 15:46:53 +0900

    22/11/10

--- js/event/designOrder.js
+++ js/event/designOrder.js
@@ -10,8 +10,23 @@ window.addEventListener('load', () => {
     const tmpId = sessionStorage.getItem('tmpId');
     const quantity = sessionStorage.getItem('quantity');
 
+    document.getElementById('ordertype').innerHTML = type +" "+ color +" "+ tmpId;
+    
+    document.getElementById('sl').innerHTML = length + "<span> mm</span>";
+    document.getElementById('sw').innerHTML = width + "<span> mm</span>";
+    document.getElementById('sd').innerHTML = depth + "<span> mm</span>";
+    document.getElementById('sth').innerHTML = thickness + "<span> mm</span>";
+    document.getElementById('sq').innerHTML = quantity + "<span> 枚</span>";
+
+    document.getElementById('price').innerHTML = depth + "<span> 円</span>";
+    document.getElementById('total').innerHTML = depth + "<span> 円</span>";
+    document.getElementById('with-tax').innerHTML = depth + '<span class="total-last-span"> 円</span>';
+
     //formに設定
-    document.getElementById('type').value = type;
+    if(type == "")
+        document.getElementById('type').value = type;
+    else
+        document.getElementById('type').value = type;
     document.getElementById('length').value = length != null ? length : '';
     document.getElementById('width').value = width != null ? width : '';
     document.getElementById('depth').value = depth != null ? depth : '';
@@ -19,5 +34,13 @@ window.addEventListener('load', () => {
     document.getElementById('color').value = color != null ? color : '';
     document.getElementById('thickness').value = thickness;
     document.getElementById('quantity').value = quantity;
+
+    console.log(type);
+    console.log(length);
+    console.log(width);
+    console.log(depth);
+    console.log(type);
+
+
 
 }, false);
