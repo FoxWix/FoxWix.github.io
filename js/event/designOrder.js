d771af658bd40016301f6eae426877b06ee3431d
AAWPE <74542941+AAWPE@users.noreply.github.com>
2022-12-20 10:28:37 +0900

    22/12/20

--- js/event/designOrder.js
+++ js/event/designOrder.js
@@ -1,17 +1,59 @@
 window.addEventListener('load', () => {
 
+    if(sessionStorage.getItem('type') == null){
+        location.href = '../../index.php';
+    }
+
     //セッションストレージから注文内容を取得
     const type = sessionStorage.getItem('type');
     const length = sessionStorage.getItem('length');
     const width = sessionStorage.getItem('width');
     const depth = sessionStorage.getItem('depth');
     const thickness = sessionStorage.getItem('thickness');
-    const color = sessionStorage.getItem('color');
+    const colorcode = sessionStorage.getItem('color');
     const tmpId = sessionStorage.getItem('tmpId');
     const quantity = sessionStorage.getItem('quantity');
+    let color;
+    let price;
+
+    switch(colorcode){
+        case "#a9a9a9":
+            color = "ダークグレー";
+            break;
+        case "#ff4500":
+            color = "オレンジレッド";
+            break;
+        case "#ffd700":
+            color = "ゴールド";
+            break;
+        case "#66cdaa":
+            color = "ミディアムカーマイン";
+            break;
+        case "#4169e1":
+            color = "ロイヤルブルー";
+            break;
+        case "#9932cc":
+            color = "ダークオーキッド";
+            break;
+    }
 
     //注文内容を設定
     if (type == 'C_order') {
+
+        //価格を設定
+        let total = Number(length) + Number(width) + Number(depth);
+        if(total <= 600){
+            price = 1000;
+        }
+        else{
+            price = 2000;
+        }
+        document.getElementById('price').innerHTML = price + "<span> 円</span>";
+        document.getElementById('total').innerHTML = (price*quantity) + "<span> 円</span>";
+        document.getElementById('with-tax').innerHTML = Math.floor(price*quantity*1.1) + '<span class="total-last-span"> 円</span>';
+
+        //　値段設定
+        document.getElementById('f_price').value = price;
 
         document.getElementById('ordertype').innerHTML = 'お客様プリント';
         document.getElementById('sl').innerHTML = length + "<span> mm</span>";
@@ -40,8 +82,34 @@ window.addEventListener('load', () => {
         }
 
 
-    } else if ('T_order') {
-        
+    } else if (type == 'T_order') {
+
+        //テンプレートデータ取得
+        fetch('../../php/get_tmpdata.php',{
+            method:'POST',
+            headers:{'Content-Type':'text/plain'},
+            body: tmpId
+        })
+        .then(response => response.text())
+        .then(res => {
+            let tmpdata = res.split(',');
+            document.getElementById('sl').innerHTML = tmpdata[0] + "<span> mm</span>";
+            document.getElementById('sw').innerHTML = tmpdata[1] + "<span> mm</span>";
+            document.getElementById('sd').innerHTML = tmpdata[2] + "<span> mm</span>";
+            document.getElementById('ordertype').innerHTML = tmpdata[4] + " " +color;
+
+            let price = tmpdata[3];
+            document.getElementById('price').innerHTML = price + "<span> 円</span>";
+            document.getElementById('total').innerHTML = (price*quantity) + "<span> 円</span>";
+            document.getElementById('with-tax').innerHTML = Math.floor(price*quantity*1.1) + '<span class="total-last-span"> 円</span>';
+
+            // form に設定
+            document.getElementById('length').value = tmpdata[0];
+            document.getElementById('width').value = tmpdata[1];
+            document.getElementById('depth').value = tmpdata[2];
+            document.getElementById('f_price').value = price;
+        })
+
         //デザインテンプレートプレビュー表示
         document.getElementById("cp").style.display = 'none';
         document.getElementById("tp").style.display = 'block';
@@ -63,8 +131,7 @@ window.addEventListener('load', () => {
 
         }
         document.getElementById('tmpImage').src = path;
-
-        document.getElementById('ordertype').innerHTML = "形状テンプレート " + color + " " + tmpId;
+        
         document.getElementById('sth').innerHTML = thickness + "<span> mm</span>";
         document.getElementById('sq').innerHTML = quantity + "<span> 枚</span>";
 
@@ -86,18 +153,17 @@ window.addEventListener('load', () => {
 
     }
 
-    //価格を設定　＊価格要修正＊
-    document.getElementById('price').innerHTML = depth + "<span> 円</span>";
-    document.getElementById('total').innerHTML = depth + "<span> 円</span>";
-    document.getElementById('with-tax').innerHTML = depth + '<span class="total-last-span"> 円</span>';
-
     //formに設定
     document.getElementById('type').value = type == '' ? '' : type;
-    document.getElementById('length').value = length != null ? length : '';
-    document.getElementById('width').value = width != null ? width : '';
-    document.getElementById('depth').value = depth != null ? depth : '';
+
+    if (type == 'C_order') {
+        document.getElementById('length').value = length != null ? length : '';
+        document.getElementById('width').value = width != null ? width : '';
+        document.getElementById('depth').value = depth != null ? depth : '';
+    }
+
     document.getElementById('tmpId').value = tmpId != null ? tmpId : '';
-    document.getElementById('color').value = color != null ? color : '';
+    document.getElementById('color').value = colorcode != null ? colorcode : '';
     document.getElementById('thickness').value = thickness;
     document.getElementById('quantity').value = quantity;
 
